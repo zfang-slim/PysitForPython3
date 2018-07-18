865348ede9a09a71099bac0d91def10e4fd58788
zfang <fzlong88@gmail.com>
2018-07-18 16:23:25 -0400

    up

--- pysit/core/mesh.py
+++ pysit/core/mesh.py
@@ -772,11 +772,11 @@ class StructuredPML(StructuredBCBase):
         # function an extra node to work with, to ensure that the delta is
         # correct, then we take that extra node off of the sigma function.
         s = domain_bc.evaluate(self._n+1, side)
-        
+
         if side == 'right':
-	       self.sigma = s[1:]
+            self.sigma = s[1:]
         elif side == 'left':
-	       self.sigma= s[:-1]
+            self.sigma= s[:-1]
 
         # Get the physical boundary type
         self._boundary_type = domain_bc.boundary_type
