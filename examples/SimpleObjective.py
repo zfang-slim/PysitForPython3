c28c36714b3c8ab6b9b72c3a0938d04f5039b59f
zfang <fzlong88@gmail.com>
2018-08-07 10:37:51 -0400

    up

--- examples/SimpleObjective.py
+++ examples/SimpleObjective.py
@@ -12,7 +12,27 @@ __docformat__ = "restructuredtext en"
 
 class SimpleExponetialObjective(ObjectiveFunctionBase):
 
+    """Class for a simple objective to test the Gradient Test Module"""
+
     def __init__(self, solver=None, parallel_wrap_shot=ParallelWrapShotNull()):
+        """Construct the simple objective class
+            We construct the following objective function:
+
+            f(x) = exp(-0.5 * |x|^2)
+
+            The corresponding gradient is
+
+            g(x) = - exp(-0.5 * |x|^2) * x
+
+            ____________________________________________________________________
+
+            In this class, the input solver and parallel_wrap_shot are not
+            necessary. The reason that we keep them is that we want to make this
+            class consistant with the objective class of pysit. So that, they can
+            be called in the same way in the Gradient Test. 
+
+        """
+
         self.parallel_wrap_shot = parallel_wrap_shot
         self.solver = solver
 
