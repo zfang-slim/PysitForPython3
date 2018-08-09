cd5af891cb8a5cf5cbac984a9ad0e251e900005a
zfang <fzlong88@gmail.com>
2018-08-08 23:49:41 -0400

    up

--- pysit/solvers/constant_density_acoustic/time/scalar/constant_density_acoustic_time_scalar_boostpython_wrapper.cpp
+++ pysit/solvers/constant_density_acoustic/time/scalar/constant_density_acoustic_time_scalar_boostpython_wrapper.cpp
@@ -38,7 +38,7 @@ BOOST_PYTHON_MODULE(_constant_density_acoustic_time_scalar_cpp)
     // // 2D
     // def("constant_density_acoustic_time_scalar_2D_4omp", constant_density_acoustic_time_scalar_2D_4omp);
     // def("constant_density_acoustic_time_scalar_2D_6omp", constant_density_acoustic_time_scalar_2D_6omp);
-    def("constant_density_acoustic_time_scalar_2D_2os", constant_density_acoustic_time_scalar_2D_2os);
+    def("constant_density_acoustic_time_scalar_2D_2os", cda_time_scalar_2D<float, 2>);
     // def("constant_density_acoustic_time_scalar_2D_4os", constant_density_acoustic_time_scalar_2D_4os);
     // def("constant_density_acoustic_time_scalar_2D_6os", constant_density_acoustic_time_scalar_2D_6os);
     // def("constant_density_acoustic_time_scalar_2D_8os", constant_density_acoustic_time_scalar_2D_8os);
@@ -46,7 +46,7 @@ BOOST_PYTHON_MODULE(_constant_density_acoustic_time_scalar_cpp)
     // // 3D
     // def("constant_density_acoustic_time_scalar_3D_4omp", constant_density_acoustic_time_scalar_3D_4omp);
     // def("constant_density_acoustic_time_scalar_3D_6omp", constant_density_acoustic_time_scalar_3D_6omp);
-    def("constant_density_acoustic_time_scalar_3D_2os", constant_density_acoustic_time_scalar_3D_2os);
+    def("constant_density_acoustic_time_scalar_3D_2os", cda_time_scalar_3D<float, 2>);
     // def("constant_density_acoustic_time_scalar_3D_4os", constant_density_acoustic_time_scalar_3D_4os);
     // def("constant_density_acoustic_time_scalar_3D_6os", constant_density_acoustic_time_scalar_3D_6os);
     // def("constant_density_acoustic_time_scalar_3D_8os", constant_density_acoustic_time_scalar_3D_8os);
