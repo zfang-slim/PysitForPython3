4d22cb6566ff17f5d065d90a496e591c01626447
zfang <fzlong88@gmail.com>
2018-08-08 22:22:10 -0400

    up

--- pysit/solvers/constant_density_acoustic/time/scalar/constant_density_acoustic_time_scalar_boostpython_wrapper.cpp
+++ pysit/solvers/constant_density_acoustic/time/scalar/constant_density_acoustic_time_scalar_boostpython_wrapper.cpp
@@ -1,32 +1,33 @@
 # include <boost/python.hpp>
-# include "constant_density_acoustic_time_scalar.i"
+// # include "constant_density_acoustic_time_scalar.i"
+# include "constant_density_acoustic_time_scalar_1D.h"
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(_constant_density_acoustic_time_scalar_cpp)
 {
     // 1D
-    def("constant_density_acoustic_time_scalar_1D_4omp", constant_density_acoustic_time_scalar_1D_4omp);
-    def("constant_density_acoustic_time_scalar_1D_6omp", constant_density_acoustic_time_scalar_1D_6omp);
-    def("constant_density_acoustic_time_scalar_1D_2os", constant_density_acoustic_time_scalar_1D_2os);
-    def("constant_density_acoustic_time_scalar_1D_4os", constant_density_acoustic_time_scalar_1D_4os);
-    def("constant_density_acoustic_time_scalar_1D_6os", constant_density_acoustic_time_scalar_1D_6os);
-    def("constant_density_acoustic_time_scalar_1D_8os", constant_density_acoustic_time_scalar_1D_8os);
-
-    // 2D
-    def("constant_density_acoustic_time_scalar_2D_4omp", constant_density_acoustic_time_scalar_2D_4omp);
-    def("constant_density_acoustic_time_scalar_2D_6omp", constant_density_acoustic_time_scalar_2D_6omp);
-    def("constant_density_acoustic_time_scalar_2D_2os", constant_density_acoustic_time_scalar_2D_2os);
-    def("constant_density_acoustic_time_scalar_2D_4os", constant_density_acoustic_time_scalar_2D_4os);
-    def("constant_density_acoustic_time_scalar_2D_6os", constant_density_acoustic_time_scalar_2D_6os);
-    def("constant_density_acoustic_time_scalar_2D_8os", constant_density_acoustic_time_scalar_2D_8os);
-
-    // 3D
-    def("constant_density_acoustic_time_scalar_3D_4omp", constant_density_acoustic_time_scalar_3D_4omp);
-    def("constant_density_acoustic_time_scalar_3D_6omp", constant_density_acoustic_time_scalar_3D_6omp);
-    def("constant_density_acoustic_time_scalar_3D_2os", constant_density_acoustic_time_scalar_3D_2os);
-    def("constant_density_acoustic_time_scalar_3D_4os", constant_density_acoustic_time_scalar_3D_4os);
-    def("constant_density_acoustic_time_scalar_3D_6os", constant_density_acoustic_time_scalar_3D_6os);
-    def("constant_density_acoustic_time_scalar_3D_8os", constant_density_acoustic_time_scalar_3D_8os);
+    // def("constant_density_acoustic_time_scalar_1D_4omp", constant_density_acoustic_time_scalar_1D_4omp);
+    // def("constant_density_acoustic_time_scalar_1D_6omp", constant_density_acoustic_time_scalar_1D_6omp);
+    def("constant_density_acoustic_time_scalar_1D_2os", cda_time_scalar_1D<float, 2>);
+    // def("constant_density_acoustic_time_scalar_1D_4os", constant_density_acoustic_time_scalar_1D_4os);
+    // def("constant_density_acoustic_time_scalar_1D_6os", constant_density_acoustic_time_scalar_1D_6os);
+    // def("constant_density_acoustic_time_scalar_1D_8os", constant_density_acoustic_time_scalar_1D_8os);
+    //
+    // // 2D
+    // def("constant_density_acoustic_time_scalar_2D_4omp", constant_density_acoustic_time_scalar_2D_4omp);
+    // def("constant_density_acoustic_time_scalar_2D_6omp", constant_density_acoustic_time_scalar_2D_6omp);
+    // def("constant_density_acoustic_time_scalar_2D_2os", constant_density_acoustic_time_scalar_2D_2os);
+    // def("constant_density_acoustic_time_scalar_2D_4os", constant_density_acoustic_time_scalar_2D_4os);
+    // def("constant_density_acoustic_time_scalar_2D_6os", constant_density_acoustic_time_scalar_2D_6os);
+    // def("constant_density_acoustic_time_scalar_2D_8os", constant_density_acoustic_time_scalar_2D_8os);
+    //
+    // // 3D
+    // def("constant_density_acoustic_time_scalar_3D_4omp", constant_density_acoustic_time_scalar_3D_4omp);
+    // def("constant_density_acoustic_time_scalar_3D_6omp", constant_density_acoustic_time_scalar_3D_6omp);
+    // def("constant_density_acoustic_time_scalar_3D_2os", constant_density_acoustic_time_scalar_3D_2os);
+    // def("constant_density_acoustic_time_scalar_3D_4os", constant_density_acoustic_time_scalar_3D_4os);
+    // def("constant_density_acoustic_time_scalar_3D_6os", constant_density_acoustic_time_scalar_3D_6os);
+    // def("constant_density_acoustic_time_scalar_3D_8os", constant_density_acoustic_time_scalar_3D_8os);
 
 
 }
