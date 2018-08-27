Zhilong Finish the modified Gradient test for both time domain and frequency domain code. right now, the gradient inside the domain is correct

examples/GradientTest_2D_Frequency.py
@@ -1,6 +1,7 @@
 # Std import block
 import time
 import copy
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,15 +9,18 @@ import matplotlib.pyplot as plt
 from pysit import *
 from pysit.gallery import horizontal_reflector
 
+
 from GradientTest import GradientTest
 
 if __name__ == '__main__':
     # Setup
+    hybrid = False
+    # enable Open MP multithread solver
+    os.environ["OMP_NUM_THREADS"] = "4"
 
     #   Define Domain
     pmlx = PML(0.1, 100)
     pmlz = PML(0.1, 100)
-
 
     x_config = (0.1, 1.0, pmlx, pmlx)
     z_config = (0.1, 0.8, pmlz, pmlz)
@@ -44,22 +48,30 @@ if __name__ == '__main__':
                                    )
 
     # Define and configure the wave solver
-    trange = (0.0, 3)
+    trange = (0.0, 3.0)
 
-    solver = ConstantDensityAcousticWave(m,
-                                         spatial_accuracy_order=2,
-                                         trange=trange,
-                                         kernel_implementation='cpp')
-
+    solver_time = ConstantDensityAcousticWave(m,
+                                              spatial_accuracy_order=6,
+                                              kernel_implementation='omp',
+                                              trange=trange)
     # Generate synthetic Seismic data
+    print('Generating data...')
+    base_model = solver_time.ModelParameters(m, {'C': C})
     tt = time.time()
-    wavefields = []
-    base_model = solver.ModelParameters(m, {'C': C})
-    generate_seismic_data(shots, solver, base_model, wavefields=wavefields)
-
+    generate_seismic_data(shots, solver_time, base_model)
     print('Data generation: {0}s'.format(time.time()-tt))
 
-    objective = TemporalLeastSquares(solver)
+    # Define and configure the objective function
+    if hybrid:
+        solver = ConstantDensityAcousticWave(m,
+                                             spatial_accuracy_order=4,
+                                             trange=trange)
+        objective = HybridLeastSquares(solver)
+    else:
+
+        solver = ConstantDensityHelmholtz(m,
+                                          spatial_accuracy_order=4)
+        objective = FrequencyLeastSquares(solver)
 
     # Define the inversion algorithm
     grad_test = GradientTest(objective)
@@ -81,11 +93,14 @@ if __name__ == '__main__':
     norm_base_model = np.linalg.norm(grad_test.base_model.data)
     dC_vec.data = dC_vec.data * 0.1 * (norm_base_model / norm_dC_vec)
     grad_test.model_perturbation = dC_vec
+
     # Execute inversion algorithm
     print('Gradient test ...')
     tt = time.time()
 
-    result = grad_test(shots)
+    objective_arguments = {'frequencies': [2.0]}
+
+    result = grad_test(shots, objective_arguments)
 
     print('...run time:  {0}s'.format(time.time()-tt))
 
@@ -106,3 +121,4 @@ if __name__ == '__main__':
 
     plt.show()
 
+    a = 1