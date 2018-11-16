deffa1b1a5cadfed3bf5ca8ed4201515d9b34b1c
zfang <fzlong88@gmail.com>
2018-11-16 00:07:10 -0500

    up

--- pysit/core/acquisition.py
+++ pysit/core/acquisition.py
@@ -111,110 +111,140 @@ def equispaced_acquisition(mesh, wavelet,
 
     return shots
 
-# def equispaced_acquisition_given_data(data, mesh, wavelet,
-#                                       odata, ddata, ndata,
-#                                       source_kwargs={},
-#                                       receiver_kwargs={},
-#                                       parallel_shot_wrap=ParallelWrapShotNull()
-#                                       ):
+def equispaced_acquisition_given_data(data, mesh, wavelet,
+                                      odata, ddata, ndata,
+                                      source_kwargs={},
+                                      receiver_kwargs={},
+                                      parallel_shot_wrap=ParallelWrapShotNull()
+                                      ):
 
 
-#     source_depth=None,
-#     receiver_depth=None,
+    source_depth=None,
+    receiver_depth=None,
 
-#     m = mesh
-#     d = mesh.domain
+    m = mesh
+    d = mesh.domain
 
-#     xmin = d.x.lbound
-#     xmax = d.x.rbound
+    xmin = d.x.lbound
+    xmax = d.x.rbound
 
-#     zmin = d.z.lbound
-#     zmax = d.z.rbound
+    zmin = d.z.lbound
+    zmax = d.z.rbound
 
-#     if m.dim == 2:
-#         data_time, data_xrec, data_zrec, data_xsrc, data_zsrc = odn2grid_data_2D_time(odata, ddata, ndata)
+    if m.dim == 2:
+        data_time, data_xrec, data_zrec, data_xsrc, data_zsrc = odn2grid_data_2D_time(odata, ddata, ndata)
 
-#     if m.dim == 3:
-#         data_time, data_xrec, data_yrec, data_zrec, data_xsrc, data_ysrc, data_zsrc = odn2grid_data_3D_time(odata, ddata, ndata)
+    if m.dim == 3:
+        data_time, data_xrec, data_yrec, data_zrec, data_xsrc, data_ysrc, data_zsrc = odn2grid_data_3D_time(odata, ddata, ndata)
 
-#     if m.dim == 3:
-#         ymin = d.y.lbound
-#         ymax = d.y.rbound
+    if m.dim == 3:
+        ymin = d.y.lbound
+        ymax = d.y.rbound
 
-#     source_depth = data_zsrc
-#     receiver_depth = data_zrec 
+    source_depth = data_zsrc
+    receiver_depth = data_zrec 
 
-#     shots = list()
+    shots = list()
 
-#     max_sources = m.x.n
+    max_sources = m.x.n
 
-#     if m.dim == 2:
-#         receivers = ndata[1]
-#         sources = ndata[3]
+    if m.dim == 2:
+        receivers = ndata[1]
+        sources = ndata[3]
 
-#         xpos_rec = data_xrec
-#         receiversbase = ReceiverSet(m, [PointReceiver(m, (x, receiver_depth), **receiver_kwargs) for x in xpos_rec])
+        xpos_rec = data_xrec
+        receiversbase = ReceiverSet(m, [PointReceiver(m, (x, receiver_depth), **receiver_kwargs) for x in xpos_rec])
 
-#         local_sources = sources / parallel_shot_wrap.size
+        if np.mod(sources, parallel_shot_wrap.size) is not 0:
+            raise('Currently, we only support the case that mod(number of sources, number of processes) = 0')
+        local_sources = sources / parallel_shot_wrap.size
 
-#     if m.dim == 3:
+    if m.dim == 3:
 
-#         receivers = (ndata[1], ndata[2])
-#         sources = (ndata[4], ndata[5])
+        receivers = (ndata[1], ndata[2])
+        sources = (ndata[4], ndata[5])
         
-#         xpos_rec = data_xrec
-#         ypos_rec = data_yrec
-#         receiversbase = ReceiverSet(m, [PointReceiver(m, (x, y, receiver_depth), **receiver_kwargs) for x in xpos_rec for y in ypos_rec])
+        xpos_rec = data_xrec
+        ypos_rec = data_yrec
+        receiversbase = ReceiverSet(m, [PointReceiver(m, (x, y, receiver_depth), **receiver_kwargs) for x in xpos_rec for y in ypos_rec])
 
-#         local_sources = np.prod(sources) / parallel_shot_wrap.size
+        if np.mod(np.prod(sources), parallel_shot_wrap.size) is not 0:
+            raise('Currently, we only support the case that mod(number of sources, number of processes) = 0')
+        local_sources = np.prod(sources) / parallel_shot_wrap.size
 
-#     print(type(local_sources))
+    print(type(local_sources))
+    local_sources = int(local_sources)
 
-#     if m.dim == 2:
-#         if parallel_shot_wrap.rank == 0:
-#             data_local = data[:,:,:,0:local_sources,:].squeeze()
+    if m.dim == 2:
+        if parallel_shot_wrap.rank == 0:
+            data_local = data[:,:,:,0:local_sources,:].squeeze()
 
-#             for i in range(1, parallel_shot_wrap.size):
-#                 data_send = data[:,:,:,i*local_sources:(i+1)*local_sources,:]
-#                 parallel_shot_wrap.comm.Send(data_send, dest=i, tag=0)
+            for i in range(1, parallel_shot_wrap.size):
+                data_send = data[:,:,:,i*local_sources:(i+1)*local_sources,:]
+                parallel_shot_wrap.comm.Send(data_send, dest=i, tag=0)
 
-#         else:
-#             parallel_shot_wrap.comm.Recv(data_receive, source=0)
-#             print('Receive data from process ', 0)
+        else:
+            parallel_shot_wrap.comm.Recv(data_receive, source=0)
+            print('Receive data from process ', 0)
 
-#             data_local = data_receive.squeeze()
+            data_local = data_receive.squeeze()
 
-#     if m.dim ==3:
-#         index_true = int(local_sources) * parallel_shot_wrap.rank + k
-#         subindex = np.unravel_index(index_true, sources)
-#         if paralle_shot_wrap.rank == 0:
+    if m.dim == 3:
+        if paralle_shot_wrap.rank == 0:
+            data_local = get_local_data(data, n, local_sources, 0)
+            for k in range(1, parallel_shot_wrap.size):
+                data_send = get_local_data(data, n, local_source, k)
+                parallel_shot_wrap.comm.Send(data_send, dest=k, tag=0)
 
-
-#     for k in range(int(local_sources)):
-#         index_true = int(local_sources) * parallel_shot_wrap.rank + k
-#         subindex = np.unravel_index(index_true, sources)
-#         idx = subindex[0]
-
-#         if m.dim == 3:
-#             jdx = subindex[1]
-
-#         if m.dim == 2:
-#             srcpos = (xmin + (xmax-xmin)*(idx+1.0)/(sources+1.0), source_depth)
-#         elif m.dim == 3:
-#             srcpos = (xmin + (xmax-xmin)*(idx+1.0)/(sources[0]+1.0), ymin + (
-#                 ymax-ymin)*(jdx+1.0)/(sources[1]+1.0), source_depth)
-
-#         # Define source location and type
-#         source = PointSource(m, srcpos, wavelet, **source_kwargs)
-
-#         # Define set of receivers
-#         receivers = copy.deepcopy(receiversbase)
-
-#         if 
-
-#         # Create and store the shot
-#         shot = Shot(source, receivers)
-#         shots.append(shot)
+        else:
+            parallel_shot_wrap.comm.Recv(data_local, source=0)
+            print('Receive data from process ', 0)
+            # data_local = np.zeros((data_time, data_xrec*data_yrec, local_sources))
+            # for k in range(local_sources):
 
 
-#     return shots
+    for k in range(int(local_sources)):
+        index_true = int(local_sources) * parallel_shot_wrap.rank + k
+        subindex = np.unravel_index(index_true, sources)
+
+        if m.dim == 2:
+            idx = subindex
+        
+        if m.dim == 3:
+            idx = subindex[0]
+            jdx = subindex[1]
+
+        if m.dim == 2:
+            srcpos = (data_xsrc[idx], source_depth)
+        elif m.dim == 3:
+            srcpos = (data_xsrc[idx], data_ysrc[jdx], source_depth)
+
+        # Define source location and type
+        source = PointSource(m, srcpos, wavelet, **source_kwargs)
+
+        # Define set of receivers
+        receivers = copy.deepcopy(receiversbase)
+
+        receivers.data = data_local[:,:,k]
+
+        # Create and store the shot
+        shot = Shot(source, receivers)
+        shots.append(shot)
+
+
+    return shots
+
+def get_local_data(data, n, local_sources, rank):
+    n_out = (n[0], n[1]*n[2], local_sources)
+    data_out = np.zeros(n_out)
+
+    for k in range(local_sources):
+        indx_k = rank * local_sources + k
+        indx_sub = np.unravel_index(indx_k, (n[4], n[5]))
+        data_tmp = np.reshape(data[:,:,:,:,indx_sub[0],indx_sub[1],:], (n[0], n[1]*n[2]))
+        data_out[:,:,k] = data_tmp
+
+    return data_out
+            
+        
+
