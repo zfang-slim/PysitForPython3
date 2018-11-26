a4d04c2aa2f36245789d163bbec781d0990ecffb
Zhilong Fang <fangzl@GREEN-FIVE-FORTY-ONE.MIT.EDU>
2018-11-26 15:09:11 -0500

    up

--- pysit/objective_functions/__init__.py
+++ pysit/objective_functions/__init__.py
@@ -4,3 +4,4 @@ from pysit.objective_functions.objective_function import *
 from pysit.objective_functions.temporal_least_squares import *
 from pysit.objective_functions.hybrid_least_squares import *
 from pysit.objective_functions.frequency_least_squares import *
+from pysit.objective_functions.temporal_least_squares_includePML import *
