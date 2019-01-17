f121cabd2220bf265aa476f5f3024bca5977ec21
Zhilong Fang <fangzl@GREEN-FIVE-FORTY-ONE.MIT.EDU>
2019-01-16 20:23:23 -0500

    up

--- pysit/objective_functions/__init__.py
+++ pysit/objective_functions/__init__.py
@@ -7,4 +7,4 @@ from pysit.objective_functions.frequency_least_squares import *
 # from pysit.objective_functions.temporal_least_squares_includePML import *
 from pysit.objective_functions.temporal_envelope import *
 from pysit.objective_functions.temporal_extended_imaging_inversion import *
-from pysit.objective_functions.temporal_extended_imaging_inversion_sub import *
+#from pysit.objective_functions.temporal_extended_imaging_inversion_sub import *
