5bf3fc7ad48e5f7a170056fb3af2fd59b805aecd
zfang-slim <fzlong88@gmail.com>
2019-06-22 20:03:23 -0400

    debug

--- pysit/optimization/PQN.py
+++ pysit/optimization/PQN.py
@@ -115,7 +115,8 @@ class PQN(OptimizationBase):
         PGDsolver = ProjectedGradientDescent(quadratic_obj, proj_op)
         if proj_op is not None:
             # initial_value_PGD = proj_op(x_k + H_BFGS.inv(-1.0*gradient))
-            initial_value_PGD = self._compute_initial_PGD(x_k, H_BFGS.inv(-1.0*gradient), proj_op)
+#             initial_value_PGD = self._compute_initial_PGD(x_k, H_BFGS.inv(-1.0*gradient), proj_op)
+            initial_value_PGD = x_k
             x_kp1, f_history, g_history, x_history = PGDsolver(
                 self.maxiter_PGD, self.maxiter_linesearch_PGD, initial_value_PGD, verbose=False)
             direction = x_kp1 - x_k
