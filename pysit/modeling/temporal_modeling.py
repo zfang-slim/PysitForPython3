9fece6e0eea2a7fe4b32b914aaf57b9816400ba7
Zhilong Fang <fangzl@Zhilongs-iMac.local>
2019-08-29 16:23:08 -0400

    up

--- pysit/modeling/temporal_modeling.py
+++ pysit/modeling/temporal_modeling.py
@@ -165,6 +165,10 @@ class TemporalModeling(object):
         if 'wavefield' in return_parameters:
             retval['wavefield'] = us
         if 'dWaveOp' in return_parameters:
+            aa = 0
+            for i in range(len(dWaveOp)):
+                aa += getsizeof(dWaveOp[i])
+            print(aa)
             retval['dWaveOp'] = dWaveOp
         if 'simdata' in return_parameters:
             retval['simdata'] = simdata
